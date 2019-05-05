c396e3882429c6907ea603ec2616ccde76de1aed
cahaug <caleahaug@gmail.com>
2019-05-05 16:02:12 -0700

    adding some event listeners throughout the page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+// document.querySelector('.form-submit').addEventListener('click', (e)=>{e.preventDefault();});
+
+document.querySelector('h2').addEventListener('mouseover', (event)=>{console.log('user mouseover title text');});
+
+window.document.addEventListener('keydown', (event)=>{console.log('user is typing');});
+
+window.document.addEventListener('wheel', (event)=>{console.log('user is scrolling');});
+
+
+const funbusHeading = document.querySelector('h1');
+funbusHeading.addEventListener('mouseover', (event)=>{console.log('user mouseover header'); funbusHeading.style.fontSize = '4.2rem'});
+
+
+
+window.addEventListener('load', (event) => {console.log('page is fully loaded');});
+
+
+
+
+// other code
+
+
